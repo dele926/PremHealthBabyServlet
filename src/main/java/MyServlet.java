3a8170da8c182d2df56b171160b691cd7fb3fc64
dele926 <adelesunnywang@gmail.com>
2020-12-19 15:22:21 +0000

    AddingDatabase

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 //This declares this program to be a servlet
 @WebServlet(urlPatterns = {"/patients"}, displayName = "Baby Health Servlet", loadOnStartup = 1)
@@ -15,9 +16,10 @@ public class MyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         //the response sent back by the server will be a JSON
         resp.setContentType("application/json");
         //Why does this response show up on my server?
-        resp.getWriter().write("Hello, world!");
+        resp.getWriter().write(reqBody);
     }
 }
