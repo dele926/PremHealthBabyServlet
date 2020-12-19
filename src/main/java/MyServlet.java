518c00db9931c9fd9cbd2c972769dccbc136766f
dele926 <adelesunnywang@gmail.com>
2020-12-19 17:05:01 +0000

    Servlet hosted locally Able to Return local SQL Result in stream

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,18 +10,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.*;
 import java.util.stream.Collectors;
 
 //This declares this program to be a servlet
 @WebServlet(urlPatterns = {"/patients"}, displayName = "Baby Health Servlet", loadOnStartup = 1)
 public class MyServlet extends HttpServlet{
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
         String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         //the response sent back by the server will be a JSON
+        //resp.setContentType("text/html");
         resp.setContentType("application/json");
-        //Why does this response show up on my server?
+        resp.getWriter().write("Thank you Client!");
         resp.getWriter().write(reqBody);
+        /*
+        Here's the SQL Query Part
+         */
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+        try {
+            // Registers the driver
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+        }
+        Connection conn= null;
+        try {
+            conn = DriverManager.getConnection(dbUrl, "postgres", "690922@Yd");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+            Statement s=conn.createStatement();
+            Gson gson = new Gson();
+            Query query = gson.fromJson(reqBody,Query.class);
+            String sqlStr = "SELECT " + query.dataRequested + ", time FROM " + "patient_"+ query.patientID + ";";
+            System.out.println(sqlStr);
+            ResultSet rset=s.executeQuery(sqlStr);
+            while(rset.next()){
+                resp.getWriter().write(rset.getDouble("glucose")+" "+ rset.getTime("time") + "\n");
+            }
+            rset.close();
+            s.close();
+            conn.close();
+        }
+        catch (Exception e){
+        }
+
     }
 }
---
+++ src/main/java/Query.java
@@ -0,0 +1,9 @@
+public class Query {
+    String patientID;
+    String dataRequested;
+    //Constructor
+    public Query (String patientID, String dataRequested){
+        this.patientID = patientID;
+        this.dataRequested = dataRequested;
+    }
+}
