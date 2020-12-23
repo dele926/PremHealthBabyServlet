1edb8623730c95d0b77720ae11160f1591517638
dele926 <adelesunnywang@gmail.com>
2020-12-23 17:48:30 +0000

    Update SQLEditClinician and SQLEditEngineer

---
+++ src/main/java/SQLConstructor/Editable.java
@@ -0,0 +1,4 @@
+package SQLConstructor;
+
+public interface Editable {
+}
---
+++ src/main/java/SQLConstructor/SQLQuery.java
@@ -0,0 +1,11 @@
+package SQLConstructor;
+
+public abstract class SQLQuery {
+    protected String type;
+
+    public SQLQuery(String type){
+        this.type = type;
+    }
+
+    public
+}
