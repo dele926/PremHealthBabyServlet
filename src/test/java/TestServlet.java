dd0e1664a170a0e0bc8aed1ca0e2045b83cdd6db
dele926 <adelesunnywang@gmail.com>
2020-12-26 21:41:03 +0000

    Update SQLEditClinician and SQLEditEngineer

--- src/test/java/TestServlet.java
+++ src/test/java/TestServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.StringWriter;
 
-
+/**
 public class TestServlet {
     @Mock
     HttpServletRequest request;
@@ -31,3 +31,5 @@ public class TestServlet {
     }
 
 }
+
+*/
