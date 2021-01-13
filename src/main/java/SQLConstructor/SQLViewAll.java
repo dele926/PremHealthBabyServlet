56c71ce2f0c772b33f43763cf2812b7be86233da
dele926 <adelesunnywang@gmail.com>
2021-01-13 23:42:03 +0000

    Update SQLViewALL to select * from pateints

--- src/main/java/SQLConstructor/SQLViewAll.java
+++ src/main/java/SQLConstructor/SQLViewAll.java
@@ -10,8 +10,7 @@ public class SQLViewAll extends SQLView implements Executeable{
     public SQLViewAll(){
         super();
         _type = "ViewAll";
-        sqlStr = "SELECT time, patient_id, sodium, potassium, lactate, glucose, potassium_input, sodium_input, " +
-                "lactate_input, glucose_input FROM patients WHERE NOT potassium_input is NULL";
+        sqlStr = "SELECT * FROM patients WHERE NOT potassium_input is NULL";
     }
 
 }
